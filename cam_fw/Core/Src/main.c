4cc04db8d9bbf60de28a4e971e323ae027630c69
Duke <d@d-l.io>
2021-03-01 16:59:30 +0800

    do not open exti0 in the cube in order to move exti0 isr to app_cam.c

--- cam_fw/Core/Src/main.c
+++ cam_fw/Core/Src/main.c
@@ -421,9 +421,6 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_Init(CD_RST_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI0_1_IRQn, 1, 0);
-  HAL_NVIC_EnableIRQ(EXTI0_1_IRQn);
-
   HAL_NVIC_SetPriority(EXTI2_3_IRQn, 2, 0);
   HAL_NVIC_EnableIRQ(EXTI2_3_IRQn);
 
--- cam_fw/Core/Src/stm32g0xx_it.c
+++ cam_fw/Core/Src/stm32g0xx_it.c
@@ -146,22 +146,6 @@ void SysTick_Handler(void)
 /******************************************************************************/
 
 /**
-  * @brief This function handles EXTI line 0 and line 1 interrupts.
-  */
-void EXTI0_1_IRQHandler(void)
-{
-  /* USER CODE BEGIN EXTI0_1_IRQn 0 */
-
-  /* USER CODE BEGIN EXTI0_1_IRQn 0 */
-
-  /* USER CODE END EXTI0_1_IRQn 0 */
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
-  /* USER CODE BEGIN EXTI0_1_IRQn 1 */
-
-  /* USER CODE END EXTI0_1_IRQn 1 */
-}
-
-/**
   * @brief This function handles EXTI line 2 and line 3 interrupts.
   */
 void EXTI2_3_IRQHandler(void)
